52c23e2969097d4899672d66d92487884862a0d0
snigdha26bhi10120-svg <snigdha.26bhi10120@vitbhopal.ac.in>
2026-09-21 15:43:44 +0530

    Update patientmanage.py

--- patientmanage.py
+++ patientmanage.py
@@ -16,3 +16,22 @@ def add_patient(patients):
     patients.append(patient)
 
     print("\nPatient added successfully!")
+
+def view_patients(patients):
+    if len(patients) == 0:
+        print("\nNo patients found.")
+        return
+
+    print("\n" + "=" * 45)
+    print("REGISTERED PATIENTS")
+    print("=" * 45)
+
+    for patient in patients:
+        print("\nPatient ID:", patient["patient_id"])
+        print("Name:", patient["name"])
+        print("Age:", patient["age"])
+        print("Gender:", patient["gender"])
+        print("Disease/Health Issue:", patient["disease"])
+        print("-" * 45)
+
+
