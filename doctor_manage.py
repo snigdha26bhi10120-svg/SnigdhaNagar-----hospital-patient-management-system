d1cb6e5f908b26bdf9e02c790a05d84196c10d79
snigdha26bhi10120-svg <snigdha.26bhi10120@vitbhopal.ac.in>
2026-09-21 19:50:43 +0530

    Update doctor_manage.py

--- doctor_manage.py
+++ doctor_manage.py
@@ -1,1 +1,50 @@
+def add_doctor(doctors):
+    doctor_id = input("Enter doctor ID: ")
+    name = input("Enter doctor name: ")
+    specialization = input("Enter specialization: ")
+    phone = input("Enter doctor phone number: ")
 
+    doctor = {
+        "doctor_id": doctor_id,
+        "name": name,
+        "specialization": specialization,
+        "phone": phone
+    }
+
+    doctors.append(doctor)
+
+    print("\nAlright, Doctor added successfully!")
+
+
+def view_doctors(doctors):
+    if len(doctors) == 0:
+        print("\nOops! No doctors found.")
+        return
+
+    print("\n" + "=" * 45)
+    print("REGISTERED DOCTORS")
+    print("=" * 45)
+
+    for doctor in doctors:
+        print("\nDoctor ID:", doctor["doctor_id"])
+        print("Name:", doctor["name"])
+        print("Specialization:", doctor["specialization"])
+        print("Phone:", doctor["phone"])
+        print("-" * 45)
+
+
+def search_doctor(doctors):
+    doctor_id = input("Enter doctor ID to search: ")
+
+    for doctor in doctors:
+        if doctor["doctor_id"] == doctor_id:
+            print("\nDoctor Found!")
+            print("-" * 30)
+            print("Doctor ID:", doctor["doctor_id"])
+            print("Name:", doctor["name"])
+            print("Specialization:", doctor["specialization"])
+            print("Phone:", doctor["phone"])
+            print("-" * 30)
+            return
+
+    print("\nDoctor not found.")
